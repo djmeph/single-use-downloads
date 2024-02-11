0f378d55b5691ba6d4d02868b32fa447f590193d
DJ Meph <info@djmeph.net>
2024-02-11 21:30:10 +0000

    Add users model lib

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUser } from './app.interface';
+import crypto from 'crypto';
+import { promisify } from 'util';
+
+const pbkdf2 = promisify(crypto.pbkdf2);
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  async createUser({ email, password }: CreateUser) {
+    const salt = crypto.randomBytes(16).toString('hex');
+    const hash = await pbkdf2(password, salt, 1000, 64, 'sha512');
+    
   }
 }
