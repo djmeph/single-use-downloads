6ab85dd4ad3792ae0c16d35227c233c7542f3e5d
DJ Meph <info@djmeph.net>
2024-02-12 00:49:48 +0000

    Add DTO for new user

--- libs/backend/controller/users/src/lib/users.dto.ts
+++ libs/backend/controller/users/src/lib/users.dto.ts
@@ -1,9 +1,22 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class UserInputDto {
   @IsEmail()
   email: string;
 
   @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(256)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   password: string;
 }
